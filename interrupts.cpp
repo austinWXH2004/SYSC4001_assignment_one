938a26aabd13d90f413bf67b7e9c3dbe5de23ee1
austinWXH2004 <austinhanwenxuan@outlook.com>
2025-09-30 22:48:22 -0400

    interrupts.cpp

--- interrupts.cpp
+++ interrupts.cpp
@@ -2,35 +2,74 @@
  *
  * @file interrupts.cpp
  * @author Sasisekhar Govind
+ * @author Tony Yao
+ * @author Wenxuan Han 101256669
  *
  */
 
-#include<interrupts.hpp>
+#include "interrupts.hpp"
+#include <tuple>
+
 
 int main(int argc, char** argv) {
 
+    std::cout << "hello world" << std::endl; 
+
     //vectors is a C++ std::vector of strings that contain the address of the ISR
-    //delays  is a C++ std::vector of ints that contain the delays of each device
+    //delays is a C++ std::vector of ints that contain the delays of each device
     //the index of these elemens is the device number, starting from 0
     auto [vectors, delays] = parse_args(argc, argv);
     std::ifstream input_file(argv[1]);
-
     std::string trace;      //!< string to store single line of trace file
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-
-
-
+    int current_time = 0;
+    int context_save_time = 10;
+    int cpu_end_time = 0;
     /******************************************************************/
 
+    
+    
     //parse each line of the input trace file
     while(std::getline(input_file, trace)) {
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
+        if (activity == "CPU") {
+            execution += std::to_string(current_time) + ", " + std::to_string(duration_intr) + ", CPU execution\n";
+            current_time += duration_intr;
+        }
+        else if (activity == "SYSCALL")
+        {
+            auto [new_execution, updated_time] = intr_boilerplate(current_time, duration_intr, context_save_time, vectors);
+            execution += new_execution; 
+            current_time = updated_time;
+        
+            execution += std::to_string(current_time) + ", " + std::to_string(delays.at(duration_intr)) + ", SYSCALL: run the ISR\n"; 
+            current_time += delays.at(duration_intr); 
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", IRET\n"; 
+            current_time ++; 
+        }
+        else if (activity == "END_IO") {
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", Check priority of interrput\n";
+            current_time++; 
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", check if masked\n";
+            current_time++; 
 
+            auto [new_execution, updated_time] = intr_boilerplate(current_time, duration_intr, context_save_time, vectors);
+            execution += new_execution;
+            current_time = updated_time; 
 
+            int device_number_delay_time = delays.at(duration_intr);
+            execution += std::to_string(current_time) + ", " + std::to_string(device_number_delay_time) + ", END_IO: run the ISR\n"; 
+            current_time += device_number_delay_time; 
+            execution += std::to_string(current_time) + ", " + std::to_string(1) + ", IRET\n"; 
+            current_time ++; 
+        }
+        else { 
+             std::cerr << "error is happened here so nothing to do unless OS let u to do work" << std::endl; 
+        }
 
         /************************************************************************/
 
